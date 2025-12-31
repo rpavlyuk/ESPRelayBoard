3cf22b36f13b25a34bf8d7af4c59a7705737e10e
Roman Pavlyuk <roman.pavlyuk@gmail.com>
2025-12-31 14:52:07 +0100

    net-logging TCP and UDP connectors rewritten for stability purposes

--- components/net-logging/tcp_client.c
+++ components/net-logging/tcp_client.c
@@ -11,18 +11,24 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <string.h>
+#include <errno.h>
+
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #if CONFIG_NET_LOGGING_USE_RINGBUFFER
 #include "freertos/ringbuf.h"
 #else
 #include "freertos/message_buffer.h"
 #endif
-#include "esp_system.h"
+
 #include "esp_log.h"
 #include "lwip/sockets.h"
-#include "netdb.h" // gethostbyname
+#include "netdb.h"
 
 #include "net_logging.h"
+
+static const char *TAG = "netlog_tcp";
 
 #if CONFIG_NET_LOGGING_USE_RINGBUFFER
 extern RingbufHandle_t xRingBufferTCP;
@@ -30,83 +36,156 @@ extern RingbufHandle_t xRingBufferTCP;
 extern MessageBufferHandle_t xMessageBufferTCP;
 #endif
 
+/**
+ * @brief Resolve hostname to IPv4 address
+ */
+static int resolve_dest_addr(const char *host, uint16_t port, struct sockaddr_in *out)
+{
+    memset(out, 0, sizeof(*out));
+    out->sin_family = AF_INET;
+    out->sin_port   = htons(port);
+
+    out->sin_addr.s_addr = inet_addr(host);
+    if (out->sin_addr.s_addr != 0xffffffff) {
+        return 0;
+    }
+
+    struct hostent *hp = gethostbyname(host);
+    if (!hp || !hp->h_addr) {
+        return -1;
+    }
+
+    struct ip4_addr *ip4_addr = (struct ip4_addr *)hp->h_addr;
+    out->sin_addr.s_addr = ip4_addr->addr;
+    return 0;
+}
+
+/**
+ * @brief Open and connect TCP socket to destination address
+ */
+static int open_and_connect(const struct sockaddr_in *dest_addr)
+{
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (sock < 0) {
+        ESP_LOGW(TAG, "socket() failed: errno=%d", errno);
+        return -1;
+    }
+
+    // Optional: avoid blocking forever on send()
+    struct timeval tv = {
+        .tv_sec = 2,
+        .tv_usec = 0
+    };
+    setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv));
+
+    int err = connect(sock, (struct sockaddr *)dest_addr, sizeof(*dest_addr)); // <-- FIXED SIZE
+    if (err != 0) {
+        ESP_LOGW(TAG, "connect() failed: errno=%d", errno);
+        close(sock);
+        return -1;
+    }
+
+    ESP_LOGI(TAG, "Connected to logging server %s:%u", inet_ntoa(dest_addr->sin_addr), ntohs(dest_addr->sin_port));
+    return sock;
+}
+
+/**
+ * @brief Send all data or fail
+ */
+static bool send_all_or_fail(int sock, const uint8_t *data, size_t len)
+{
+    size_t off = 0;
+    while (off < len) {
+        int ret = send(sock, data + off, len - off, 0);
+        if (ret > 0) {
+            off += (size_t)ret;
+            continue;
+        }
+
+        if (ret == 0) {
+            ESP_LOGW(TAG, "send() returned 0 (peer closed?)");
+            return false;
+        }
+
+        // ret < 0
+        ESP_LOGW(TAG, "send() failed: errno=%d", errno);
+        return false;
+    }
+    return true;
+}
+
+/**
+ * @brief TCP client task
+ */
 void tcp_client(void *pvParameters)
 {
-	PARAMETER_t *task_parameter = pvParameters;
-	PARAMETER_t param;
-	memcpy((char *)&param, task_parameter, sizeof(PARAMETER_t));
-	printf("Start:param.port=%d param.ipv4=[%s]\n", param.port, param.ipv4);
+    PARAMETER_t param;
+    memcpy(&param, pvParameters, sizeof(param));
 
-	int addr_family = 0;
-	int ip_protocol = 0;
+    ESP_LOGI(TAG, "Start: %s:%u", param.ipv4, (unsigned)param.port);
 
-	struct sockaddr_in dest_addr;
-	dest_addr.sin_addr.s_addr = inet_addr(param.ipv4);
-	dest_addr.sin_family = AF_INET;
-	dest_addr.sin_port = htons(param.port);
-	addr_family = AF_INET;
-	ip_protocol = IPPROTO_IP;
+    struct sockaddr_in dest_addr;
+    if (resolve_dest_addr(param.ipv4, param.port, &dest_addr) != 0) {
+        ESP_LOGE(TAG, "DNS/addr resolve failed for host '%s'", param.ipv4);
+        vTaskDelete(NULL);
+        return;
+    }
 
-	printf("dest_addr.sin_addr.s_addr=0x%"PRIx32"\n", dest_addr.sin_addr.s_addr);
-	if (dest_addr.sin_addr.s_addr == 0xffffffff) {
-		struct hostent *hp;
-		hp = gethostbyname(param.ipv4);
-		if (hp == NULL) {
-			printf("FTP Client Error: Connect, gethostbyname\n");
-			vTaskDelete(NULL);
-		}
-		struct ip4_addr *ip4_addr;
-		ip4_addr = (struct ip4_addr *)hp->h_addr;
-		dest_addr.sin_addr.s_addr = ip4_addr->addr;
-		printf("dest_addr.sin_addr.s_addr=0x%"PRIx32"\n", dest_addr.sin_addr.s_addr);
-	}
-	
+    // Reconnect loop with backoff
+    int sock = -1;
+    int backoff_ms = 250;
+    const int backoff_max_ms = 5000;
 
-	int sock = socket(addr_family, SOCK_STREAM, ip_protocol);
-	if (sock < 0) {
-		//ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-		vTaskDelete(NULL);
-	}
-	printf("Socket created, connecting to %s:%d\n", param.ipv4, param.port);
+    while (sock < 0) {
+        sock = open_and_connect(&dest_addr);
+        if (sock >= 0) break;
 
-	int err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in6));
-	if (err == 0) {
-		printf("Successfully connected\n");
-	} else {
-		printf("Socket unable to connect: errno %d\n", errno);
-		vTaskDelete(NULL);
-	}
+        vTaskDelay(pdMS_TO_TICKS(backoff_ms));
+        if (backoff_ms < backoff_max_ms) backoff_ms *= 2;
+        if (backoff_ms > backoff_max_ms) backoff_ms = backoff_max_ms;
+    }
 
-	// Send ready to receive notify
-	xTaskNotifyGive(param.taskHandle);
+    // Tell producer we are ready to receive
+    xTaskNotifyGive(param.taskHandle);
 
-	while (1) {
+    for (;;) {
 #if CONFIG_NET_LOGGING_USE_RINGBUFFER
-        size_t received;
+        size_t received = 0;
         char *buffer = (char *)xRingbufferReceive(xRingBufferTCP, &received, portMAX_DELAY);
-        //printf("xRingBufferReceive received=%d\n", received);
-#else
-		char buffer[xItemSize];
-		size_t received = xMessageBufferReceive(xMessageBufferTCP, buffer, sizeof(buffer), portMAX_DELAY);
-		//printf("xMessageBufferReceive received=%d\n", received);
-#endif
-		if (received > 0) {
-			//printf("xMessageBufferReceive buffer=[%.*s]\n",received, buffer);
-			int ret = send(sock, buffer, received, 0);
-			LWIP_ASSERT("ret == received", ret == received);
-#if CONFIG_NET_LOGGING_USE_RINGBUFFER
-            vRingbufferReturnItem(xRingBufferTCP, (void *)buffer);
-#endif
-		} else {
-			//printf("xMessageBufferReceive fail\n");
-			break;
-		} // end if
-	} // end while
+        if (!buffer || received == 0) {
+            continue;
+        }
 
-	if (sock != -1) {
-		//ESP_LOGE(TAG, "Shutting down socket and restarting...");
-		shutdown(sock, 0);
-		close(sock);
-	}
-	vTaskDelete(NULL);
+        // IMPORTANT: ringbuffer item must be returned. If you want retry, you must copy.
+        bool ok = send_all_or_fail(sock, (const uint8_t *)buffer, received);
+        vRingbufferReturnItem(xRingBufferTCP, (void *)buffer);
+
+#else
+        char buffer[xItemSize];
+        size_t received = xMessageBufferReceive(xMessageBufferTCP, buffer, sizeof(buffer), portMAX_DELAY);
+        if (received == 0) {
+            continue;
+        }
+        bool ok = send_all_or_fail(sock, (const uint8_t *)buffer, received);
+#endif
+
+        if (!ok) {
+            // Connection likely broken: close and reconnect, but DO NOT crash device.
+            if (sock != -1) {
+                shutdown(sock, 0);
+                close(sock);
+                sock = -1;
+            }
+
+            // reconnect with backoff
+            int bo = 250;
+            while (sock < 0) {
+                sock = open_and_connect(&dest_addr);
+                if (sock >= 0) break;
+                vTaskDelay(pdMS_TO_TICKS(bo));
+                if (bo < backoff_max_ms) bo *= 2;
+                if (bo > backoff_max_ms) bo = backoff_max_ms;
+            }
+        }
+    }
 }
--- components/net-logging/udp_client.c
+++ components/net-logging/udp_client.c
@@ -1,27 +1,34 @@
 /*
-	UDP Client
+    UDP Client - hardened for logging use
 
-	This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-	Unless required by applicable law or agreed to in writing, this
-	software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-	CONDITIONS OF ANY KIND, either express or implied.
+    Key changes:
+    - no LWIP_ASSERT() on normal network errors
+    - supports hostname resolution (gethostbyname)
+    - recreates socket on sendto() failures with backoff
+    - always returns ringbuffer items
 */
 
 #include <stdio.h>
 #include <inttypes.h>
 #include <string.h>
+#include <errno.h>
+
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #if CONFIG_NET_LOGGING_USE_RINGBUFFER
 #include "freertos/ringbuf.h"
 #else
 #include "freertos/message_buffer.h"
 #endif
-#include "esp_system.h"
+
 #include "esp_log.h"
 #include "lwip/sockets.h"
+#include "netdb.h"
 
 #include "net_logging.h"
+
+static const char *TAG = "netlog_udp";
 
 #if CONFIG_NET_LOGGING_USE_RINGBUFFER
 extern RingbufHandle_t xRingBufferUDP;
@@ -29,103 +36,138 @@ extern RingbufHandle_t xRingBufferUDP;
 extern MessageBufferHandle_t xMessageBufferUDP;
 #endif
 
-void udp_dump(char *id, char *data, int len)
+// Resolve IPv4 dotted string or hostname into sockaddr_in
+static int resolve_dest_addr(const char *host, uint16_t port, struct sockaddr_in *out)
 {
-  int i;
-  printf("[%s]\n",id);
-  for(i=0;i<len;i++) {
-    printf("%0x ",data[i]);
-    if ( (i % 10) == 9) printf("\n");
-  }
-  printf("\n");
+    memset(out, 0, sizeof(*out));
+    out->sin_family = AF_INET;
+    out->sin_port = htons(port);
+
+    // Try literal IPv4 first
+    out->sin_addr.s_addr = inet_addr(host);
+    if (out->sin_addr.s_addr != 0xffffffff) {
+        return 0;
+    }
+
+    // Fallback to DNS
+    struct hostent *hp = gethostbyname(host);
+    if (!hp || !hp->h_addr) {
+        return -1;
+    }
+
+    struct ip4_addr *ip4_addr = (struct ip4_addr *)hp->h_addr;
+    out->sin_addr.s_addr = ip4_addr->addr;
+    return 0;
 }
 
-// UDP Client Task
-void udp_client(void *pvParameters) {
-	PARAMETER_t *task_parameter = pvParameters;
-	PARAMETER_t param;
-	memcpy((char *)&param, task_parameter, sizeof(PARAMETER_t));
-	//printf("Start:param.port=%d param.ipv4=[%s]\n", param.port, param.ipv4);
+static int open_udp_socket(void)
+{
+    int fd = lwip_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (fd < 0) {
+        ESP_LOGW(TAG, "socket() failed: errno=%d", errno);
+        return -1;
+    }
 
-	struct sockaddr_in addr;
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(param.port);
-	//addr.sin_addr.s_addr = htonl(INADDR_BROADCAST); /* send message to 255.255.255.255 */
-	//addr.sin_addr.s_addr = inet_addr("255.255.255.255"); /* send message to 255.255.255.255 */
-	addr.sin_addr.s_addr = inet_addr(param.ipv4);
+    // Optional: avoid any surprising long blocks
+    struct timeval tv = {.tv_sec = 2, .tv_usec = 0};
+    (void)setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv));
 
-	/* create the socket */
-	int fd;
-	int ret;
-	fd = lwip_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP ); // Create a UDP socket.
-	LWIP_ASSERT("fd >= 0", fd >= 0);
+    return fd;
+}
 
-	// Send ready to receive notify
-	xTaskNotifyGive(param.taskHandle);
+void udp_client(void *pvParameters)
+{
+    PARAMETER_t param;
+    memcpy(&param, pvParameters, sizeof(PARAMETER_t));
 
-	while(1) {
+    ESP_LOGI(TAG, "Start: %s:%u", param.ipv4, (unsigned)param.port);
+
+    struct sockaddr_in addr;
+    if (resolve_dest_addr(param.ipv4, param.port, &addr) != 0) {
+        ESP_LOGE(TAG, "DNS/addr resolve failed for host '%s'", param.ipv4);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    // Create socket with backoff
+    int fd = -1;
+    int backoff_ms = 250;
+    const int backoff_max_ms = 5000;
+
+    while (fd < 0) {
+        fd = open_udp_socket();
+        if (fd >= 0) break;
+
+        vTaskDelay(pdMS_TO_TICKS(backoff_ms));
+        if (backoff_ms < backoff_max_ms) backoff_ms *= 2;
+        if (backoff_ms > backoff_max_ms) backoff_ms = backoff_max_ms;
+    }
+
+    // Ready to receive
+    xTaskNotifyGive(param.taskHandle);
+
+    int send_fail_streak = 0;
+
+    while (1) {
 #if CONFIG_NET_LOGGING_USE_RINGBUFFER
-		size_t received;
-		char *buffer = (char *)xRingbufferReceive(xRingBufferUDP, &received, portMAX_DELAY);
-		//printf("xRingBufferReceive received=%d\n", received);
+        size_t received = 0;
+        char *buffer = (char *)xRingbufferReceive(xRingBufferUDP, &received, portMAX_DELAY);
+        if (!buffer || received == 0) {
+            continue;
+        }
 #else
-		char buffer[xItemSize];
-		size_t received = xMessageBufferReceive(xMessageBufferUDP, buffer, sizeof(buffer), portMAX_DELAY);
-		//printf("xMessageBufferReceive received=%d\n", received);
+        char buffer[xItemSize];
+        size_t received = xMessageBufferReceive(xMessageBufferUDP, buffer, sizeof(buffer), portMAX_DELAY);
+        if (received == 0) {
+            continue;
+        }
 #endif
-		if (received > 0) {
-			//printf("xMessageBufferReceive buffer=[%.*s]\n",received, buffer);
-			//udp_dump("buffer", buffer, received);
-			ret = lwip_sendto(fd, buffer, received, 0, (struct sockaddr *)&addr, sizeof(addr));
-			LWIP_ASSERT("ret == received", ret == received);
+
+        // UDP: sendto() should send full datagram or fail, but we still handle short returns safely
+        int ret = lwip_sendto(fd, buffer, received, 0, (struct sockaddr *)&addr, sizeof(addr));
+
 #if CONFIG_NET_LOGGING_USE_RINGBUFFER
-			vRingbufferReturnItem(xRingBufferUDP, (void *)buffer);
+        // Always return the item regardless of send result
+        vRingbufferReturnItem(xRingBufferUDP, (void *)buffer);
 #endif
-		} else {
-			printf("xMessageBufferReceive fail\n");
-			break;
-		}
-	} // end while
 
-/*
-buffer included escape code
-[buffer]
-1b 5b 30 3b 33 32 6d 49 20 28
-36 31 32 33 29 20 4d 41 49 4e
-3a 20 63 68 69 70 20 6d 6f 64
-65 6c 20 69 73 20 31 2c 20 1b
-5b 30 6d a
+        if (ret == (int)received) {
+            send_fail_streak = 0;
+            continue;
+        }
 
-ESC [ 0 ; 3 2 m I SP (
-6 1 2 3 ) SP M A I N
-: SP c h i p SO m o d
-e l SP i s SP 1 , SP ESC
-[ 0 m LF
+        if (ret < 0) {
+            ESP_LOGW(TAG, "sendto() failed: errno=%d (streak=%d)", errno, send_fail_streak + 1);
+        } else {
+            ESP_LOGW(TAG, "sendto() short write: ret=%d expected=%u (streak=%d)",
+                     ret, (unsigned)received, send_fail_streak + 1);
+        }
 
-I (6123) MAIN: chip model is 1,
+        send_fail_streak++;
 
-Start coloring:
-ESC [ 0 ; 3 2 m
-ESC [ ColorCode m
-Finished coloring:
-ESC [ 0 m
+        // Close and recreate socket on failures
+        if (fd != -1) {
+            (void)lwip_close(fd);
+            fd = -1;
+        }
 
-buffer NOT included escape code
-[buffer]
-3c 62 61 2d 61 64 64 3e 69 64
-78 3a 31 20 28 69 66 78 3a 30
-2c 20 66 38 3a 62 37 3a 39 37
-3a 33 36 3a 64 65 3a 35 32 29
-2c 20 74 69 64 3a 30 2c 20 73
-73 6e 3a 34 2c 20 77 69 6e 53
-69 7a 65 3a 36 34
-*/
+        // If failures persist, re-resolve hostname periodically (helps with DNS/IP changes)
+        if ((send_fail_streak % 10) == 0) {
+            if (resolve_dest_addr(param.ipv4, param.port, &addr) != 0) {
+                ESP_LOGW(TAG, "Re-resolve failed for '%s' (will keep retrying)", param.ipv4);
+            } else {
+                ESP_LOGI(TAG, "Re-resolved destination '%s'", param.ipv4);
+            }
+        }
 
-	// Close socket
-	ret = lwip_close(fd);
-	LWIP_ASSERT("ret == 0", ret == 0);
-	vTaskDelete( NULL );
-
+        // Recreate socket with backoff
+        int bo = 250;
+        while (fd < 0) {
+            fd = open_udp_socket();
+            if (fd >= 0) break;
+            vTaskDelay(pdMS_TO_TICKS(bo));
+            if (bo < backoff_max_ms) bo *= 2;
+            if (bo > backoff_max_ms) bo = backoff_max_ms;
+        }
+    }
 }
-
